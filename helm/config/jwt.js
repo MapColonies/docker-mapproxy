ca9e2df1a35dd7b4f7ff5193be7e1c7c9eace2cc
root <shlomiko@rafael.co.il>
2023-05-08 14:11:38 +0300

    fix: token payload test

--- helm/config/jwt.js
+++ helm/config/jwt.js
@@ -1,16 +1,13 @@
-// function jwt(data) {
-//     var parts = data.split('.').slice(0,2)
-//         .map(v=>Buffer.from(v, 'base64url').toString())
-//         .map(JSON.parse);
-//     return { headers:parts[0], payload: parts[1] };
-// }
-
-// function jwt_payload_sub(r) {
-//     return jwt(r.headersIn.Authorization.slice(7)).payload.sub;
-// }
-
-function hello(r) {
-    r.return(200, "Hello world!\n");
+function jwt(data) {
+    var parts = data.split('.').slice(0,2)
+        .map(v=>Buffer.from(v, 'base64url').toString())
+        .map(JSON.parse);
+    return { headers:parts[0], payload: parts[1] };
 }
 
-export default {hello}
+function jwt_payload_sub(r) {
+    return jwt(r.headersIn.Authorization.slice(7)).payload.sub;
+}
+
+
+export default {jwt_payload_sub}
